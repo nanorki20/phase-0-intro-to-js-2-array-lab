b1257268dfb0d3d7d9b8f459ccc90f3f0635fa35
Hayet Kumasseh <naaakleh@gmail.com>
2022-05-20 13:28:45 -0400

    commit tree bcffd7eb2977a40e4132647e693857d56282e309

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Write your solution here!
+
+const cats = ['Milo', 'Otis', 'Garfield']
+beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+  });
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+  }
+destructivelyAppendCat('Ralph')
+  
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+  }
+destructivelyPrependCat('Bob')
+
+function destructivelyRemoveLastCat() {
+      cats.pop()
+  }
+destructivelyRemoveLastCat()
+
+function destructivelyRemoveFirstCat() {
+      cats.shift()
+  }
+destructivelyRemoveFirstCat()
+
+function appendCat(name) {
+    let newCats = [...cats, 'Broom']
+    return newCats
+  }
+appendCat()
+
+function prependCat(name) {
+     let newArray = ['Arnold', ...cats]
+     return newArray
+  }
+prependCat()
+
+function removeLastCat() {
+      let newArray = cats.slice(0, cats.length-1)
+      return newArray
+  }
+removeLastCat() 
+
+
+function removeFirstCat() {
+    let newArray = cats.slice(1)
+    return newArray
+}
+removeFirstCat()
